4e4bd77ddff95efe97a63aa6b691d0b944ac1c5c
DharshiniT <tkp.dharshini@gmail.com>
2017-06-09 15:23:16 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class even
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%2==0)
+		System.out.println("even");
+		
+		else
+		System.out.println("odd");
+	}
+	
+}
